83fdf27b59e1dd286d37b013d98c658b61aa62e9
haybre <haybreapril@gmail.com>
2026-07-25 16:50:53 +0200

    started BaseTest class

---
+++ src/test/java/Base/BaseTest.java
@@ -0,0 +1,26 @@
+package Base;
+
+import Utilities.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import org.testng.annotations.BeforeClass;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class BaseTest {
+
+    protected AppiumDriver driver;
+    protected Properties config;
+
+    @BeforeClass
+    public void setUpAndLogin() throws IOException {
+        config = new Properties();
+        FileInputStream fis = new FileInputStream(
+                System.getProperty("user.dir") + "/src/test/resources/configs/config.properties");
+        config.load(fis);
+
+        DriverFactory.initDriver(config);
+        driver = DriverFactory.getDriver();
+    }
+}
